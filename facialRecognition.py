8339838f7594df4f87739e4c60d8f3acf11000a1
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-09-18 23:39:15 -0400

    optimization stuff

--- facialRecognition.py
+++ facialRecognition.py
@@ -1,11 +1,13 @@
 import argparse
 import string
+import threading
 from pathlib import Path
 
 import cv2
 import face_recognition
 import pickle
 from collections import Counter
+import time
 from PIL import Image, ImageDraw
 import numpy as np
 
@@ -35,7 +37,7 @@ parser.add_argument(
     help="Which model to use for training: HOG(cpu intensive) preferred"
 )
 parser.add_argument(
-    "-f", action="store",  help="Image Path"
+    "-f", action="store", help="Image Path"
 )
 args = parser.parse_args()
 
@@ -91,7 +93,7 @@ def recognize_faces(
     # draw = ImageDraw.Draw(pillow_image)
 
     # load image in cv2 img obj
-    img = cv2.imread(image_location,cv2.IMREAD_COLOR)
+    img = cv2.imread(image_location, cv2.IMREAD_COLOR)
 
     # compare new encodings to known encodings from training data
     # bounding box is perceived area around face detected
@@ -108,14 +110,52 @@ def recognize_faces(
     # pillow_image.show()
 
 
-def _display_face(img, bounding_box, name):
-    # draw rectangle on face and stuff
+def recognize_face_live(
+        frame,
+        model: str = "hog",
+        encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+
+    input_face_locations = face_recognition.face_locations(
+        frame, model=model
+    )
+    input_face_encodings = face_recognition.face_encodings(
+        frame, input_face_locations
+    )
+
+    for bounding_box, unknown_encoding in zip(
+            input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        display_face_live(frame, bounding_box, name)
+
+
+def display_face_live(img, bounding_box, name):
     top, right, bottom, left = bounding_box
-    cv2.rectangle(img, (left, top), (right,bottom), (0,0,255), 10)
+    cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 4)
     # add name text
     font = cv2.FONT_HERSHEY_SIMPLEX
     uppercase_name = name.upper()
-    cv2.putText(img, uppercase_name, (left, bottom), font, 1, (200,255,155), 2, cv2.LINE_AA)
+    # img = cv2.flip(img, 1)
+    cv2.putText(img, uppercase_name, (left, bottom), font, 0.5, (200, 255, 155), 1, cv2.LINE_AA)
+    # cv2.imshow("video", img)
+
+
+def _display_face(img, bounding_box, name):
+    # draw rectangle on face and stuff
+    top, right, bottom, left = bounding_box
+    cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 6)
+
+    # add name text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    uppercase_name = name.upper()
+    # img = cv2.flip(img, 1)
+    cv2.putText(img, uppercase_name, (left, bottom), font, 1, (200, 255, 155), 2, cv2.LINE_AA)
+
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -147,7 +187,54 @@ def validate(model: str = "hog"):
             )
 
 
+def live_facial_recognition():
+    camera = cv2.VideoCapture(0)
+
+    if not camera.isOpened():
+        raise IOError("Cannot open video source")
+
+    do_facial_recognition = True
+
+    pTime = 0
+
+    counter = 0
+
+    while True:
+        ret, frame = camera.read()
+
+        frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
+        # use boolean to only do every other frame (cpu will die)
+        if counter % 30 == 0:
+            recognize_face_live(frame)
+        else:
+            if counter > 1230:
+                counter = 0
+            else:
+                counter += 1
+
+        # recognize_face_live(frame)
+
+        # instantiate thread to speed up
+        # threading.Thread(target=recognize_face_live, args=(frame.copy(),)).start()
+        # frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(frame, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow("video", frame)
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
+
+    global face_match
+    global bounding_box
+
     if args.train:
         encode_known_faces(model=args.m)
     elif args.validate:
@@ -157,7 +244,7 @@ if __name__ == "__main__":
     elif args.doiwork:
         print("yes, i am working")
     elif args.live:
-        print("Starting Camera Feed...")
+        live_facial_recognition()
     else:
         # general run format (no args)
         user_input = int(input("Mode (1 for img, 2 for live): "))
@@ -165,6 +252,6 @@ if __name__ == "__main__":
             user_input = input("Enter Image Path: ")
             recognize_faces(image_location=user_input)
         elif user_input == 2:
-            print("i haven't made this yet lolz")
+            live_facial_recognition()
         else:
             print("Invalid input")
