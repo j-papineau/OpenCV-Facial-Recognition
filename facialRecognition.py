14f3bd32d70172019b6e058ab54535eab361e258
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-09-19 19:50:00 -0400

    major optimization changes

--- facialRecognition.py
+++ facialRecognition.py
@@ -12,6 +12,7 @@ from PIL import Image, ImageDraw
 import numpy as np
 import os
 from sys import platform
+from recognizeChad import *
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 BOUNDING_BOX_COLOR = "red"
@@ -19,6 +20,7 @@ TEXT_COLOR = "white"
 
 parser = argparse.ArgumentParser(description="Facial Recognition Project")
 parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument("--train_emotion", action="store_true", help="Train on input data for emotions")
 parser.add_argument(
     "--validate", action="store_true", help="Run Validation Script (many windows will open)"
 )
@@ -127,6 +129,9 @@ def recognize_face_live(
         frame, input_face_locations
     )
 
+    # init empty array of bounding boxes to return mult faces
+    # bounding_boxes = []
+
     for bounding_box, unknown_encoding in zip(
             input_face_locations, input_face_encodings
     ):
@@ -135,6 +140,8 @@ def recognize_face_live(
             name = "Unknown"
         if not bounding_box:
             bounding_box = ""
+        
+        # print(bounding_box)
         return bounding_box, name
 
 
@@ -197,16 +204,17 @@ def live_facial_recognition():
     print("camera opened successfully")
     pTime = 0
     counter = 0
+    chad = False
 
     while True:
         ret, frame = camera.read()
 
         frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # use boolean to only do every other frame (cpu will die)
+        # use boolean or counter to only do every other frame (cpu will die)
         if counter % 10 == 0:
-            # TODO: refactor: return bounding box so it can be printed over and over
             res = recognize_face_live(frame)
+            # chad = recognize_chad(frame)
             if res:
                 bounding_box = res[0]
                 name = res[1]
@@ -219,11 +227,6 @@ def live_facial_recognition():
 
         if counter > 100000:
             counter = 0
-            
-        # recognize_face_live(frame)
-
-        # instantiate thread to speed up
-        # threading.Thread(target=recognize_face_live, args=(frame.copy(),)).start()
         
         # draw stuff on image
         frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
@@ -232,6 +235,7 @@ def live_facial_recognition():
             cv2.rectangle(frame, (left * 2,top * 2), (right * 2, bottom * 2), (0, 50, 255), 2)
             font = cv2.FONT_HERSHEY_COMPLEX
             cv2.putText(frame, name, (left * 2, bottom * 2), font, 0.5, (255,255, 255), 1, cv2.LINE_AA)
+            # cv2.putText(frame, f'Sigma: {chad}', (left * 2, (bottom * 2) + 20), font, 0.5, (255,255, 255), 1, cv2.LINE_AA)
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
@@ -246,11 +250,107 @@ def live_facial_recognition():
     cv2.destroyAllWindows()
 
 
+def load_known_encodings():
+    pass
+
+# optimize to take loaded_encodings as a param
+def recognize_multiple_faces_live():
+    with DEFAULT_ENCODINGS_PATH.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+    
+    print("attempting to start video captue")
+
+    # OS camera statement
+    if platform == "darwin":
+        camera = cv2.VideoCapture(0)
+    elif platform == "win32":
+        camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    if not camera.isOpened():
+        raise IOError("Cannot open video source")
+    
+    pTime = 0
+    counter = 0
+
+    # load known encodings from pickle
+
+    if loaded_encodings:
+        print("facial encodings loaded")
+
+    face_data = []
+
+    while True:
+        ret, frame = camera.read()
+
+        frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if counter % 10 == 0:
+            face_data = recognize_multiple_faces(frame, loaded_encodings)
+        else:
+            pass
+        counter += 1
+
+        if counter > 100000:
+            counter = 0
+
+        frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+
+        if face_data:
+            for face in face_data:
+                name = face[0]
+                box = face[1]
+                top, right, bottom, left = box
+                cv2.rectangle(frame, (left * 2,top * 2), (right * 2, bottom * 2), (0, 50, 255), 2)
+                font = cv2.FONT_HERSHEY_COMPLEX
+                cv2.putText(frame, name, (left * 2, bottom * 2), font, 0.5, (255,255, 255), 1, cv2.LINE_AA)
+        
+        # fps calculation
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+    
+        cv2.putText(frame, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow("video", frame)
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+    
+
+def recognize_multiple_faces(
+        frame,
+        encodings,
+        model: str = "hog",
+) -> None:
+    
+    face_data = []
+
+    input_face_locations = face_recognition.face_locations(
+        frame, model=model
+    )   
+    input_face_encodings = face_recognition.face_encodings(
+        frame, input_face_locations
+    )
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, encodings)
+        if not name:
+            name = "Unknown"
+        if not bounding_box:
+            bounding_box = ""
+        face_data.append([name, bounding_box])
+    return face_data
+
+
 if __name__ == "__main__":
 
     global face_match
-    global bounding_box
-
+    
     if args.train:
         encode_known_faces(model=args.m)
     elif args.validate:
@@ -261,6 +361,8 @@ if __name__ == "__main__":
         print("yes, i am working")
     elif args.live:
         live_facial_recognition()
+    elif args.train_emotion:
+        print("training emotion off known data")
     else:
         # general run format (no args)
         user_input = int(input("Mode (1 for img, 2 for live): "))
@@ -268,6 +370,14 @@ if __name__ == "__main__":
             user_input = input("Enter Image Path: ")
             recognize_faces(image_location=user_input)
         elif user_input == 2:
-            live_facial_recognition()
+            mode = int(input("Facial Recognition Routine\n 1.) General Live Routine \n 2.) Multiple Faces \n 3.) With Emotion\n"))
+            if mode == 1:
+                live_facial_recognition()
+            elif mode == 2:
+                recognize_multiple_faces_live()
+            elif mode == 3:
+                pass
+            else:
+                print("invalid")
         else:
             print("Invalid input")
---
+++ recognizeChad.py
@@ -0,0 +1,4 @@
+# code for recognizing that detected face is a chad
+
+def recognize_chad(frame):
+    return False
